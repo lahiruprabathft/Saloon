e183132ce6bb601c8468f3e025a89ec906b8df40
Lahiru Prabath <lahiruprabathft@gmail.com>
2024-02-22 22:05:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function showPopup() {
+    var popup = document.getElementById("myPopup");
+    popup.style.display = "block";
+}
+
+function hidePopup() {
+    var popup = document.getElementById("myPopup");
+    popup.style.display = "none";
+}
+
+
+
+function submitform (){
+     document.getElementById("successMessage").style.display = "block";
+     return false;
+ 
+}
+
+
